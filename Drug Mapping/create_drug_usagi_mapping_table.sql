1d950c98bc1b4f0695b06c848b493751639bf784
capuhcheeno <66498688+capuhcheeno@users.noreply.github.com>
2024-05-21 18:52:58 -0400

    Added table creation script from ltscomputingllc/faersdbstats

---
+++ Drug Mapping/create_drug_usagi_mapping_table.sql
@@ -0,0 +1,11 @@
+set search_path = faers;
+
+drop table if exists drug_usagi_mapping;
+CREATE TABLE drug_usagi_mapping
+(
+  drug_name_original text,
+  concept_name character varying(1000),
+  concept_class_id character varying(500),
+  concept_id integer,
+  update_method text
+)
