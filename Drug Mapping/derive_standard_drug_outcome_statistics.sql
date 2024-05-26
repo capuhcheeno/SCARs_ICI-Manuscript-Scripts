e670879ce44a21376c09ab0f3148dee0320c496a
capuhcheeno <66498688+capuhcheeno@users.noreply.github.com>
2024-05-26 16:00:21 -0400

    Update derive_standard_drug_outcome_statistics.sql

--- Drug Mapping/derive_standard_drug_outcome_statistics.sql
+++ Drug Mapping/derive_standard_drug_outcome_statistics.sql
@@ -7,29 +7,48 @@ SELECT
     outcome_concept_id, 
     CAST(NULL AS INTEGER) AS snomed_outcome_concept_id,
     count_a AS case_count,
-    CASE 
-        WHEN (count_c = 0 OR (count_a + count_b) = 0 OR (count_c + count_d) = 0 OR count_a <= 0 OR count_b <= 0 OR count_c <= 0 OR count_d <= 0) THEN NULL 
-        ELSE ROUND(((count_a::float / (count_a + count_b)) / (count_c::float / (count_c + count_d)))::numeric, 5) 
-    END AS prr,
-    CASE 
-        WHEN (count_c = 0 OR (count_a + count_b) = 0 OR (count_c + count_d) = 0 OR count_a <= 0 OR count_b <= 0 OR count_c <= 0 OR count_d <= 0) THEN NULL 
-        ELSE ROUND(EXP(LN(((count_a::float / (count_a + count_b)) / (count_c::float / (count_c + count_d)))) + 1.96 * SQRT((1.0 / count_a) - (1.0 / (count_a + count_b)) + (1.0 / count_c) - (1.0 / (count_c + count_d))))::numeric, 5)
-    END AS prr_95_percent_upper_confidence_limit,
-    CASE 
-        WHEN (count_c = 0 OR (count_a + count_b) = 0 OR (count_c + count_d) = 0 OR count_a <= 0 OR count_b <= 0 OR count_c <= 0 OR count_d <= 0) THEN NULL 
-        ELSE ROUND(EXP(LN(((count_a::float / (count_a + count_b)) / (count_c::float / (count_c + count_d)))) - 1.96 * SQRT((1.0 / count_a) - (1.0 / (count_a + count_b)) + (1.0 / count_c) - (1.0 / (count_c + count_d))))::numeric, 5)
-    END AS prr_95_percent_lower_confidence_limit,
-    CASE 
-        WHEN (count_c = 0 OR count_d = 0 OR count_b = 0 OR count_a = 0 OR count_a <= 0 OR count_b <= 0 OR count_c <= 0 OR count_d <= 0) THEN NULL 
-        ELSE ROUND(((count_a::float / count_c) / (count_b::float / count_d))::numeric, 5)
-    END AS ror,
-    CASE 
-        WHEN (count_c = 0 OR count_d = 0 OR count_b = 0 OR count_a = 0 OR count_a <= 0 OR count_b <= 0 OR count_c <= 0 OR count_d <= 0) THEN NULL 
-        ELSE ROUND(EXP(LN(((count_a::float / count_c) / (count_b::float / count_d))) + 1.96 * SQRT((1.0 / count_a) + (1.0 / count_b) + (1.0 / count_c) + (1.0 / count_d)))::numeric, 5)
-    END AS ror_95_percent_upper_confidence_limit,
-    CASE 
-        WHEN (count_c = 0 OR count_d = 0 OR count_b = 0 OR count_a = 0 OR count_a <= 0 OR count_b <= 0 OR count_c <= 0 OR count_d <= 0) THEN NULL 
-        ELSE ROUND(EXP(LN(((count_a::float / count_c) / (count_b::float / count_d))) - 1.96 * SQRT((1.0 / count_a) + (1.0 / count_b) + (1.0 / count_c) + (1.0 / count_d)))::numeric, 5)
-    END AS ror_95_percent_lower_confidence_limit
+    ROUND(
+        CASE 
+            WHEN (count_a + count_b) = 0 OR (count_c + count_d) = 0 THEN NULL
+            ELSE (count_a / (count_a + count_b)) / (count_c / (count_c + count_d)) 
+        END, 
+    5) AS prr,
+    ROUND(
+        CASE 
+            WHEN (count_a + count_b) = 0 OR (count_c + count_d) = 0 OR count_a = 0 THEN NULL
+            ELSE EXP(LN((count_a / (count_a + count_b)) / (count_c / (count_c + count_d))) + 1.96 * SQRT((1.0 / count_a) - (1.0 / (count_a + count_b)) + (1.0 / count_c) - (1.0 / (count_c + count_d)))) 
+        END, 
+    5) AS prr_95_percent_upper_confidence_limit,
+    ROUND(
+        CASE 
+            WHEN (count_a + count_b) = 0 OR (count_c + count_d) = 0 OR count_a = 0 THEN NULL
+            ELSE EXP(LN((count_a / (count_a + count_b)) / (count_c / (count_c + count_d))) - 1.96 * SQRT((1.0 / count_a) - (1.0 / (count_a + count_b)) + (1.0 / count_c) - (1.0 / (count_c + count_d)))) 
+        END, 
+    5) AS prr_95_percent_lower_confidence_limit,
+    ROUND(
+        CASE 
+            WHEN count_c = 0 OR count_d = 0 THEN NULL
+            ELSE (count_a / count_c) / (count_b / count_d) 
+        END, 
+    5) AS ror,
+    ROUND(
+        CASE 
+            WHEN count_c = 0 OR count_d = 0 OR count_a = 0 OR count_b = 0 THEN NULL
+            ELSE EXP(LN((count_a / count_c) / (count_b / count_d)) + 1.96 * SQRT((1.0 / count_a) + (1.0 / count_b) + (1.0 / count_c) + (1.0 / count_d))) 
+        END, 
+    5) AS ror_95_percent_upper_confidence_limit,
+    ROUND(
+        CASE 
+            WHEN count_c = 0 OR count_d = 0 OR count_a = 0 OR count_b = 0 THEN NULL
+            ELSE EXP(LN((count_a / count_c) / (count_b / count_d)) - 1.96 * SQRT((1.0 / count_a) + (1.0 / count_b) + (1.0 / count_c) + (1.0 / count_d))) 
+        END, 
+    5) AS ror_95_percent_lower_confidence_limit
 FROM 
-    standard_drug_outcome_contingency_table;
+    standard_drug_outcome_contingency_table
+WHERE 
+    count_a > 0 
+    AND count_b > 0 
+    AND count_c > 0 
+    AND count_d > 0 
+    AND (count_a + count_b) > 0 
+    AND (count_c + count_d) > 0;
