98616950560da13ce448bfa3d23fc4678d2e542d
capuhcheeno <66498688+capuhcheeno@users.noreply.github.com>
2024-06-12 22:49:08 -0400

    Update derive_standard_drug_outcome_statistics.sql

--- Drug Mapping/derive_standard_drug_outcome_statistics.sql
+++ Drug Mapping/derive_standard_drug_outcome_statistics.sql
@@ -1,54 +1,14 @@
-SET search_path = faers;
+set search_path = faers;
 
-DROP TABLE IF EXISTS standard_drug_outcome_statistics;
-CREATE TABLE standard_drug_outcome_statistics AS
-SELECT 
-    drug_concept_id, 
-    outcome_concept_id, 
-    CAST(NULL AS INTEGER) AS snomed_outcome_concept_id,
-    count_a AS case_count,
-    ROUND(
-        CASE 
-            WHEN (count_a + count_b) = 0 OR (count_c + count_d) = 0 THEN NULL
-            ELSE (count_a / (count_a + count_b)) / (count_c / (count_c + count_d)) 
-        END, 
-    5) AS prr,
-    ROUND(
-        CASE 
-            WHEN (count_a + count_b) = 0 OR (count_c + count_d) = 0 OR count_a = 0 THEN NULL
-            ELSE EXP(LN((count_a / (count_a + count_b)) / (count_c / (count_c + count_d))) + 1.96 * SQRT((1.0 / count_a) - (1.0 / (count_a + count_b)) + (1.0 / count_c) - (1.0 / (count_c + count_d)))) 
-        END, 
-    5) AS prr_95_percent_upper_confidence_limit,
-    ROUND(
-        CASE 
-            WHEN (count_a + count_b) = 0 OR (count_c + count_d) = 0 OR count_a = 0 THEN NULL
-            ELSE EXP(LN((count_a / (count_a + count_b)) / (count_c / (count_c + count_d))) - 1.96 * SQRT((1.0 / count_a) - (1.0 / (count_a + count_b)) + (1.0 / count_c) - (1.0 / (count_c + count_d)))) 
-        END, 
-    5) AS prr_95_percent_lower_confidence_limit,
-    ROUND(
-        CASE 
-            WHEN count_c = 0 OR count_d = 0 THEN NULL
-            ELSE (count_a / count_c) / (count_b / count_d) 
-        END, 
-    5) AS ror,
-    ROUND(
-        CASE 
-            WHEN count_c = 0 OR count_d = 0 OR count_a = 0 OR count_b = 0 THEN NULL
-            ELSE EXP(LN((count_a / count_c) / (count_b / count_d)) + 1.96 * SQRT((1.0 / count_a) + (1.0 / count_b) + (1.0 / count_c) + (1.0 / count_d))) 
-        END, 
-    5) AS ror_95_percent_upper_confidence_limit,
-    ROUND(
-        CASE 
-            WHEN count_c = 0 OR count_d = 0 OR count_a = 0 OR count_b = 0 THEN NULL
-            ELSE EXP(LN((count_a / count_c) / (count_b / count_d)) - 1.96 * SQRT((1.0 / count_a) + (1.0 / count_b) + (1.0 / count_c) + (1.0 / count_d))) 
-        END, 
-    5) AS ror_95_percent_lower_confidence_limit
-FROM 
-    standard_drug_outcome_contingency_table
-WHERE 
-    count_a > 0 
-    AND count_b > 0 
-    AND count_c > 0 
-    AND count_d > 0 
-    AND (count_a + count_b) > 0 
-    AND (count_c + count_d) > 0;
+drop table if exists standard_drug_outcome_statistics;
+create table standard_drug_outcome_statistics as
+select 
+drug_concept_id, outcome_concept_id, cast(null as integer) as snomed_outcome_concept_id,
+count_a as case_count,
+round((count_a / (count_a + count_b)) / (count_c / (count_c + count_d)),5) as prr,
+round(exp(ln((count_a / (count_a + count_b)) / (count_c / (count_c + count_d)))+1.96*sqrt((1.0/count_a)-(1.0/(count_a+count_b))+(1.0/count_c)-(1.0/(count_c+count_d)))),5) as prr_95_percent_upper_confidence_limit,
+round(exp(ln((count_a / (count_a + count_b)) / (count_c / (count_c + count_d)))-1.96*sqrt((1.0/count_a)-(1.0/(count_a+count_b))+(1.0/count_c)-(1.0/(count_c+count_d)))),5) as prr_95_percent_lower_confidence_limit,
+round(((count_a / count_c) / (count_b / count_d)),5) as ror,
+round(exp((ln((count_a / count_c) / (count_b / count_d)))+1.96*sqrt((1.0/count_a)+(1.0/count_b)+(1.0/count_c)+(1.0/count_d))),5) as ror_95_percent_upper_confidence_limit,
+round(exp((ln((count_a / count_c) / (count_b / count_d)))-1.96*sqrt((1.0/count_a)+(1.0/count_b)+(1.0/count_c)+(1.0/count_d))),5) as ror_95_percent_lower_confidence_limit
+from standard_drug_outcome_contingency_table;
