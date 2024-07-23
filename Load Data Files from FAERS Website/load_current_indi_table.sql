798bc598a0d99f60cd478f2be1b15289371c21dc
capuhcheeno <66498688+capuhcheeno@users.noreply.github.com>
2024-07-23 15:34:19 -0400

    Update load_current_indi_table.sql

--- Load Data Files from FAERS Website/load_current_indi_table.sql
+++ Load Data Files from FAERS Website/load_current_indi_table.sql
@@ -18,6 +18,6 @@ filename varchar
 );
 truncate indi;
 
-COPY indi FROM '/home/ubuntu/inbound_data/faers/ascii/all_indi_data_with_filename.txt' WITH DELIMITER E'$' CSV HEADER QUOTE E'\b' ;
+COPY indi FROM 'D:\2004-2016\all_indi_data_with_filename.txt' WITH DELIMITER E'$' CSV HEADER QUOTE E'\b' ;
 select filename, count(*) from indi group by filename order by 1;
 
