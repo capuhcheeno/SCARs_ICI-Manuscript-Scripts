c0b5e3d8cf1b95b8cb54c2460edff4949d17c37c
capuhcheeno <66498688+capuhcheeno@users.noreply.github.com>
2024-07-23 15:37:52 -0400

    Update load_legacy_rpsr_table.sql

--- Load Data Files from FAERS Website/load_legacy_rpsr_table.sql
+++ Load Data Files from FAERS Website/load_legacy_rpsr_table.sql
@@ -16,5 +16,5 @@ FILENAME varchar
 );
 truncate rpsr_legacy;
 
-COPY rpsr_legacy FROM '/home/lee/data/inbound/faers/legacy/ascii/all_rpsr_legacy_data_with_filename.txt' WITH DELIMITER E'$' CSV HEADER QUOTE E'\b' ;
+COPY rpsr_legacy FROM 'D:\Legacy\all_rpsr_legacy_data_with_filename.txt' WITH DELIMITER E'$' CSV HEADER QUOTE E'\b' ;
 select distinct filename from rpsr_legacy order by 1 ;
